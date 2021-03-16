fa7633b71971e176057d0bc5591e8ad0c04eee00
Nicolas Castillo <castillonicolas606@gmail.com>
2021-03-16 00:13:13 +0000

    fix the betty in the file printf_int.c

--- printf_int.c
+++ printf_int.c
@@ -1,6 +1,7 @@
 #include "holberton.h"
+
 /**
- * len_dig: calculates the number of digits that an integer contains.
+ * len_dig - calculates the number of digits that an integer contains.
  *
  * @num: The number to measure.
  * Return: The number of digits that num has.
@@ -18,25 +19,26 @@ int len_dig(int num)
 	if (num == 0)
 		return (1);
 
-	while(sum / 10 > 0) // 1; 0
+	while (sum / 10 > 0)
 	{
-	        sum = num; // 10 ;  1
-	        num = num / 10; //1; 0
-	        digitos += 1; //1; 2
+		sum = num;
+		num = num / 10;
+		digitos += 1;
 	}
 
-	return(digitos);
+	return (digitos);
 }
 
 /**
- * print_int: Converts each digit of an integer to characters
+ * print_int - Converts each digit of an integer to characters
  * and places them in a buffer array from the indicated position.
  *
- * @num: The number to convert.
+ * @a: The number to convert.
  * @index: The position where buffer carry on.
  * @buffer: The array to be filled.
  * Return: The number of digits converted.
  */
+
 int print_int(va_list a, char *buffer, int index)
 {
 	int num = va_arg(a, int);
@@ -57,7 +59,7 @@ int print_int(va_list a, char *buffer, int index)
 
 	i = len;
 
-	while(i > ctrl)
+	while (i > ctrl)
 	{
 		convert[i - 1] = (num % 10) + '0';
 		num = num / 10;
@@ -74,70 +76,72 @@ int print_int(va_list a, char *buffer, int index)
 
 	free(convert);
 
-	return(len);
+	return (len);
 
 }
 
 /**
- * len_u_dig: calculates the number of digits that an unsigned int data contains.
- * 
+ * len_u_dig: calculates the number of digits that an unsigned int data
+ * contains.
+ *
  * @num: The number to measure.
  * Return: The number of digits that num has.
  */
 
 int len_u_dig(unsigned int num)
 {
-    unsigned int sum = 11;
-    int digitos = 0;
+	unsigned int sum = 11;
+	int digitos = 0;
 
-    if (num == 0)
-        return (1);
+	if (num == 0)
+		return (1);
 
-    while(sum / 10 > 0) // 1; 0
-    {   
-        sum = num; // 10 ;  1
-        num = num / 10; //1; 0
-        digitos += 1; //1; 2
-    }
+	while (sum / 10 > 0)
+	{
+		sum = num;
+		num = num / 10;
+		digitos += 1;
+	}
 
-    return(digitos);
+	return (digitos);
 }
 
 /**
- * print_u_int: Converts each digit of an unsigned integer to characters
+ * print_u_int - Converts each digit of an integer to characters
  * and places them in a buffer array from the indicated position.
  *
- * @num: The number to convert.
+ * @a: The number to convert.
  * @index: The position where buffer carry on.
  * @buffer: The array to be filled.
  * Return: The number of digits converted.
  */
+
 int print_u_int(va_list a, char *buffer, int index)
 {
-    unsigned int num = va_arg(a, unsigned int);
-    int len, i = 0, ctrl = 0;
-    char *convert;
+	unsigned int num = va_arg(a, unsigned int);
+	int len, i = 0, ctrl = 0;
+	char *convert;
 
-    len = len_u_dig(num);
-    
-    convert = malloc(len);
+	len = len_u_dig(num);
 
-    i = len;
-    
-    while(i > ctrl)
-    {
-        convert[i - 1] = (num % 10) + '0';
-        num = num / 10;
-        i--;
-    }
+	convert = malloc(len);
 
-    for (i = 0; i < len; i++)
-    {
-        buffer[index] = convert[i];
-        index++;
-    }
-    
-    free(convert);
+	i = len;
 
-    return(len);
+	while (i > ctrl)
+	{
+		convert[i - 1] = (num % 10) + '0';
+		num = num / 10;
+		i--;
+	}
+
+	for (i = 0; i < len; i++)
+	{
+		buffer[index] = convert[i];
+		index++;
+	}
+
+	free(convert);
+
+	return (len);
 }
