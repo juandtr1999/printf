653496ec0c888d83f5e5c4f3f0557ee6a8f4b569
juandtr1999 <jr9282507@gmail.com>
2021-03-16 23:10:08 -0500

    converts decimal to binary

---
+++ printf_base.c
@@ -0,0 +1,95 @@
+#include "holberton.h"
+/**
+ * converts decimal to binary by
+ * using unsigned int decimal va_arg(a, unsigned
+ */
+
+int print_binary(va_list a, char * buffer, int index)
+{
+  unsigned int decimal = va_arg(a, unsigned int);
+
+  int i = 0, j, l;
+  int *k;
+  unsigned int tmp = decimal;
+
+  while(tmp > 0)
+  {
+    tmp /= 2;
+    i++;
+  }
+ 
+  k = malloc(i);
+  
+  if(k == 0)
+    return (0);
+    
+  i = 0;
+  while(decimal > 0)
+  {
+    k[i] = decimal % 2;
+    decimal /= 2;
+    i++;
+  }
+
+  for(j = i - 1; j >= 0; --j, l++)
+    {
+        buffer[index] = k[j] + '0';
+        index += 1;
+    }
+  free (k);
+  return (i); 
+}
+
+int print_octal(va_list a, char * buffer, int index)
+{
+    
+  unsigned int octal = va_arg(a, unsigned int);
+  
+  int i = 0, j, l; 
+  int *k;
+  unsigned int tmp = octal;
+  
+  while(tmp > 0)
+  {
+    tmp /= 8;
+    i++;
+  }
+  
+  k = malloc(i);
+  
+  if(k == 0)
+    return (0);
+    
+  i = 0;
+  while(octal > 0)
+  {
+    k[i] = octal % 8;
+    octal /= 8;
+    i++;
+  }
+  
+  for(j = i - 1; j >= 0; --j, l++)
+    {
+        buffer[index] = k[j] + '0';
+        index += 1;
+    }
+  free (k);
+  return (i); 
+}
+
+
+int print_x_hexal(va_list a, char * buffer, int index)
+{
+    
+  unsigned int hexal = va_arg(a, unsigned int);
+  
+  int i = 0, j, l; 
+  int *k;
+  unsigned int tmp = hexal;
+  
+  while(tmp > 0)
+  {
+    tmp /= 16;
+    i++;
+  }
+  
